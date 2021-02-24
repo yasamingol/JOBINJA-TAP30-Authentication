ca4ba2eff227acaf64f4c4e5108273cb7bc3374d
tapsi <yasamingol>
2021-02-24 17:10:13 +0330

    decoding is not working

--- Server/Controller/Authentication.py
+++ Server/Controller/Authentication.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
-from datetime import date, datetime
+from datetime import date, datetime, time
 import jwt
-import json
+import asyncio
 
 # server requests
 app = Flask(__name__)
@@ -16,16 +16,25 @@ def login():
 
 
 # token
-def createToken():
+def generateJWT():
     user = {
         "username": "jafar",
         "password": "1234"
     }
-    payload = {"some": "payload", "aud": user}
-    encoded = jwt.encode(payload, "secret")
-    return encoded
+    jwt_valid_seconds = 180
+    expiryTime = round(datetime.now().timestamp()) + jwt_valid_seconds
+    payload = {"some": "payload", "aud": user,"exp":expiryTime}
+    token = jwt.encode(payload, "secret")
+    return token
 
+
+def checkIfTokenIsExpired(token):
+    decodedToken = jwt.decode(token, "secret")
+    currentTime = round(datetime.now().timestamp())
+    tokenExp = decodedToken.exp
+    return currentTime>tokenExp
 
 if __name__ == '__main__':
     # app.run(host="127.0.0.1", port="5000")
-    print(createToken())
+    token = generateJWT()
+    print(checkIfTokenIsExpired(token))
