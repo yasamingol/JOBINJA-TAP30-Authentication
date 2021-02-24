cb2c71c8c84024a48e01e536fe70d1fea6a01486
tapsi <yasamingol>
2021-02-24 14:13:06 +0330

    post in falsk working properly - jwt not working

--- Server/Controller/Authentication.py
+++ Server/Controller/Authentication.py
@@ -1,25 +1,39 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request
+from datetime import datetime
+import jwt
+
 app = Flask(__name__)
 
 accountUserName = None
 accountPassWord = None
 loginToken = None
 
+
 @app.route('/login',methods=['POST'])
 def login():
     if request.method == 'POST':
         accountUserName = request.json.get('username')
         accountPassWord = request.json.get('password')
-        return createToken(accountUserName,accountPassWord)
+        return accountPassWord+accountUserName
+
+
+def createToken(username, password):
+    try:
+        user = {
+            'username': username,
+            'password': password,
+            'time': datetime.now()
+        }
+        return jwt.encode(
+            user,
+            app.config.get('SECRET_KEY'),
+            algorithm='HS256'
+        )
+    except Exception as e:
+        return e
 
 
 
-
-def createToken(username,password):
-    #actual token creator
-    loginToken = username+password
-    return loginToken
 
 if __name__ == '__main__':
-    app.run(host= "127.0.0.1",port="5000")
-
+    app.run(host="127.0.0.1", port="5000")
