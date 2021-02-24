7cc17112cbdb3bc74fc79186e5519fd484618fc3
tapsi <yasamingol>
2021-02-24 12:05:55 +0330

    simple flask methods

--- Server/Controller/Authentication.py
+++ Server/Controller/Authentication.py
@@ -1,6 +1,26 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+accountUserName = None
+accountPassWord = None
+
+
+
+@app.route('/postUsername/<string:username>')
+def postUserName(username):
+    # return escape(username)
+    accountUserName = username
+    return accountUserName
+
+
+
+
+@app.route('/postPassword/<string:password>')
+def postUserPassword(password):
+    accountPassWord = password
+    return accountPassWord
+
+
+if __name__ == '__main__':
+    app.run(host= "127.0.0.1",port="5000")
+
