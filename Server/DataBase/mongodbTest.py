56deb6158bb37ddeca736b84821b2f3b76ac5829
tapsi <yasamingol>
2021-03-03 13:13:28 +0330

    testing mongoDB

--- Server/DataBase/mongodbTest.py
+++ Server/DataBase/mongodbTest.py
@@ -4,6 +4,17 @@ cluster = MongoClient("mongodb+srv://yasamingol:2431380@cluster0.rrxyk.mongodb.n
 db = cluster["test"]
 collection = db["test"]
 
-post = {"_id": 0, "name": "tim", "score": 5}
+post1 = {"_id": 1, "name": "tim", "score": 5}
+post2 = {"_id": 2, "name": "yac", "score": 10}
 
-collection.insert_one(post)
+# collection.insert_many([post1, post2])
+# results = collection.find({"name": "yac"})
+# results = collection.find_one({"_id": ""})
+# results = collection.delete_one({})
+# results = collections.update_one({"_id":1},{"$set":{"name":"jafar"}})
+# for result in results:
+#     print(result)
+#     print(result["_id"])
+
+post_count = collection.count_documents({})
+print(post_count)
