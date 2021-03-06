d9826e54d71d119cfb69a2879fdf54b1d3de75b4
tapsi <yasamingol>
2021-03-06 14:26:30 +0330

    almost everything is working properly in Account

--- Server/DataBase/Model/Account*.py
+++ Server/DataBase/Model/Account*.py
@@ -2,18 +2,43 @@ from pymongo import MongoClient
 from mongoengine import *
 import asyncio
 
-
-db = (connect('Accounts', host='mongodb+srv://yasamingol:2431380@cluster0.rrxyk.mongodb.net/Accounts?retryWrites=true&w=majority'))
+db = (connect('Accounts',
+              host='mongodb+srv://yasamingol:2431380@cluster0.rrxyk.mongodb.net/Accounts?retryWrites=true&w=majority'))
 
 
 class Account(Document):
+    _id = StringField(required=True)
     username = StringField(required=True)
     password = StringField(required=True)
     meta = {'collection': 'accounts'}
 
-async def saveAccount(username,password):
-    Account(username=username, password=password).save()
+
+async def saveAccount(username, password):
+    Account(username=username, password=password, _id="1").save()
     print("account saved to DB successfully")
 
 
+async def getAccountsFullDBTable():
+    return Account.objects()
 
+
+async def getFullAccountById(accountId):
+    account = Account.objects(_id=accountId)[0]
+    return account
+
+
+async def getAccountUsernameUsingAccountId(accountID):
+    account = Account.objects(_id=accountID)[0]
+    return account.username
+
+async def getAccountPasswordUsingAccountId(accountID):
+    account = Account.objects(_id=accountID)[0]
+    return account.password
+
+async def getAccountIDUsingAccountUsername(accountUsername) :
+    account = Account.objects(username=accountUsername)[0]
+    return account._id
+
+
+# asyncio.run(saveAccount("yasamingol","2431380"))
+print(asyncio.run(getAccountIDUsingAccountUsername("yasamingol")))
