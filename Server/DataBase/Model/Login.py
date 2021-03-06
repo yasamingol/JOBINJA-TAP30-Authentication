1ce27ad76aadea1792b82a0a53f4b947dc298bbf
tapsi <yasamingol>
2021-03-06 14:45:25 +0330

    only getLastLoginTokenId not working properly

---
+++ Server/DataBase/Model/Login.py
@@ -0,0 +1,51 @@
+from pymongo import MongoClient
+from mongoengine import *
+import asyncio
+
+db = (connect('Accounts',
+              host='mongodb+srv://yasamingol:2431380@cluster0.rrxyk.mongodb.net/Accounts?retryWrites=true&w=majority'))
+
+
+class Login(Document):
+    _id = StringField(required=True)
+    username = StringField(required=True)
+    loginToken = StringField(required=True)
+    loginTime = StringField(required=True)
+    meta = {'collection': 'logins'}
+
+
+async def saveLogin(username, loginToken, loginTime):
+    id = await getNumberOfRowsOfLoginsTable()
+    Login(username=username, loginToken=loginToken, loginTime=loginTime, _id=str(id + 1)).save()
+    print("account logged in successfully")
+
+
+async def getNumberOfRowsOfLoginsTable():
+    numberOfLogins = Login.objects().count()
+    return numberOfLogins
+
+
+async def getLoginsFullDBTable():
+    return Login.objects
+
+
+async def getLoginIdUsingToken(token):
+    login = Login.objects(loginToken=token)[0]
+    return login._id
+
+async def getAccountIdUsingToken(token):
+    login = Login.objects(loginToken = token)[0]
+    return login.username
+
+async def getLastLoginTokenId(username):
+    result = Login.objects(username=username).sort([("_id", -1)]).limit(1)
+    return result
+
+async def checkIfTokenExists(token):
+    result = Login.objects(loginToken= token)
+    return result
+
+
+# asyncio.run(saveLogin("yasamingol","token","time"))
+print(asyncio.run(checkIfTokenExists("todfljken")))
+
